f79e8728f4c1565aca8ab59d69e93c575d1bea09
bom0320 <s24040@gsm.hs.kr>
2025-03-19 16:03:38 +0900

    update: 이벤트 처리하기

--- front/App.tsx
+++ front/App.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, View, Text, StyleSheet, SafeAreaView, TextInput } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 const App = () => {
+  const [name, setName] = useState('');
+
+
+  const handleChangeInput = (text: string) => {
+    console.log(text);
+    setName(text);
+  }
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
         <Text style={styles.text}>React Native</Text>
-        <TextInput style={styles.input} />
+        <TextInput style={styles.input} onChangeText={handleChangeInput} />
       </View>
-      <View style={styles.inputContainer}>
-        <Text style={styles.text}>React Native</Text>
-        <TextInput style={styles.input} />
-      </View>
-
     </SafeAreaView>
 
   );
@@ -22,7 +23,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'yellow',
   },
   text: {
     fontSize: 18,
@@ -34,11 +34,10 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     borderColor: 'black',
     width: 100,
-    height: 100,
+    height: 50,
   },
   inputContainer: {
     flex: 1,
-    backgroundColor: 'red',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center'
