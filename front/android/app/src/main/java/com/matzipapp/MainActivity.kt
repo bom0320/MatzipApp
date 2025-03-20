f31b813055d1cc2ca69b6e5897308ff93e54df31
bom0320 <s24040@gsm.hs.kr>
2025-03-20 21:13:25 +0900

    fix: Stack Navigator

--- front/android/app/src/main/java/com/matzipapp/MainActivity.kt
+++ front/android/app/src/main/java/com/matzipapp/MainActivity.kt
@@ -1,32 +1,19 @@
-package com.matzipapp;
+package com.matzipapp
 
-import com.facebook.react.ReactActivity;
-import android.os.Bundle;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle
+import com.facebook.react.ReactActivity
+import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactActivityDelegate
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "MatzipApp"
+    override fun getMainComponentName(): String = "MatzipApp"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
-}
- 
- public class MainActivity extends ReactActivity {
-  // ...
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(null);
-  }
-  // ...
+    override fun createReactActivityDelegate(): ReactActivityDelegate =
+        DefaultReactActivityDelegate(this, mainComponentName, DefaultNewArchitectureEntryPoint.fabricEnabled)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(null)
+    }
 }
