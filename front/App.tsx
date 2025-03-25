8a8b503edc36e3eedb813fb505702ecbdc4997b8
bom0320 <s24040@gsm.hs.kr>
2025-03-25 20:19:22 +0900

    update:  스크린 구조 및 옵션 설정

--- front/App.tsx
+++ front/App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import AuthHomeScreen from './src/screens/AuthHomeScreen';
+import AuthHomeScreen from './src/screens/auth/AuthHomeScreen';
 import AuthStackNavigator from './src/navigations/stack/AuthStackNavigator';
 import RootNavigator from './src/navigations/root/RootNavigator';
 function App() {
--- front/src/navigations/stack/AuthStackNavigator.tsx
+++ front/src/navigations/stack/AuthStackNavigator.tsx
@@ -2,10 +2,10 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { Button, StyleSheet, View, Text } from 'react-native';
-import AuthHomeScreen from '../../screens/AuthHomeScreen';
-import LoginScreen from '../../screens/LoginScreen';
+import AuthHomeScreen from '../../screens/auth/AuthHomeScreen';
+import LoginScreen from '../../screens/auth/LoginScreen';
 import { authNavigations } from '../../constants';
-import SignupScreen from '../../screens/SignupScreen';
+import SignupScreen from '../../screens/auth/SignupScreen';
 
 export type AuthStackParamList = {
     [authNavigations.AUTH_HOME]: undefined;
@@ -13,14 +13,33 @@ export type AuthStackParamList = {
     [authNavigations.SIGNUP]: undefined;
 }
 
+const Stack = createStackNavigator<AuthStackParamList>();
 
 function AuthStackNavigator() {
-    const Stack = createStackNavigator<AuthStackParamList>();
     return (
-        <Stack.Navigator>
-            <Stack.Screen name={authNavigations.AUTH_HOME} component={AuthHomeScreen} />
-            <Stack.Screen name={authNavigations.LOGIN} component={LoginScreen} />
-            <Stack.Screen name={authNavigations.SIGNUP} component={SignupScreen} />
+        <Stack.Navigator screenOptions={{
+            cardStyle: {
+                backgroundColor: 'white'
+            },
+            headerStyle: {
+                backgroundColor: 'white',
+                shadowColor: 'gray',
+            },
+            headerTitleStyle: {
+                fontSize: 20,
+            },
+            headerTintColor: 'black',
+        }}>
+            <Stack.Screen name={authNavigations.AUTH_HOME} component={AuthHomeScreen} options={{
+                headerTitle: ' ',
+                headerShown: false,
+            }} />
+            <Stack.Screen name={authNavigations.LOGIN} component={LoginScreen} options={{
+                headerTitle: '로그인'
+            }} />
+            <Stack.Screen name={authNavigations.SIGNUP} component={SignupScreen} options={{
+                headerTitle: '회원가입'
+            }} />
         </Stack.Navigator>
     );
 }
